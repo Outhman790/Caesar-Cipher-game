2795c0fe315b544e62bd438038137a9dc312e7f8
Outhman Moumou <moumououthman@gmail.com>
2024-04-08 03:40:28 +0000

    Made some enhancements to the code

--- Ceaser_Cipher_game.py
+++ Ceaser_Cipher_game.py
@@ -1,41 +1,13 @@
 from logo import Logo
 from functions import *
-alphabet_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 isGameFinished = False
 
 while isGameFinished == False:
     print(Logo)
-    encode_or_decode = input("Type 'encode' to encrypt or type 'decode' to decrypt: \n")
-    # Logic for encoding a word
-    if encode_or_decode == "encode":
-        text = input("Type your message: \n").lower()
-        shift = int(input("Type the shift number: "))
-        text_arr= list(text)
-        encrypted_word_arr = []
-        for letter in range(len(text_arr)):
-            for i in range(len(alphabet_letters)):
-                if text_arr[letter] == alphabet_letters[i]:
-                    encrypted_word_arr.append(alphabet_letters[i+shift])
-        print(f"{"".join(encrypted_word_arr)}")
-        playAgain(isGameFinished)
+    encode_or_decode = input("Type 'encode' to encrypt or type 'decode' to decrypt: \n").lower()
+    ceaserCipher(encode_or_decode,isGameFinished)
        
-    # Logic for decoding a word
-    elif encode_or_decode == "decode":
-        text = input("Type your message: \n").lower()
-        shift = int(input("Type the shift number: "))
-        text_arr= list(text)
-        decrypted_word_arr = []
-        for letter in range(len(text_arr)):
-            for i in range(len(alphabet_letters)):
-                if text_arr[letter] == alphabet_letters[i]:
-                    decrypted_word_arr.append(alphabet_letters[i-shift])
-        print(f"{"".join(decrypted_word_arr)}")
-        playAgain(isGameFinished)
-
-    # User wrote something different than encode or decode
-    else:
-        print("Please write either encode or decode")
-        isGameFinished = False
+   
 
   
 
--- functions.py
+++ functions.py
@@ -1,7 +1,41 @@
 import subprocess
 import os
 import sys
+alphabet_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+# A function asking the user if he wants to play again 
+def playAgain(isGameFinished):
+    play_again = input("Type yes if you wanna play again, no for closing the game: ").lower()
+    while play_again != "yes" and play_again != "no":
+            play_again = input("Type yes if you wanna play again, no for closing the game: ").lower()
+    if play_again == "yes":
+            isGameFinished = False
+            # Clear screen and play again
+            clear_screen()
+    elif play_again == "no":
+            isGameFinished = True
+            # Close the game in terminal
+            sys.exit()
+            
+
+def ceaserCipher(encodeordecode,isGameFinished):
+        if encodeordecode == "encode" or encodeordecode == "decode":
+            text = input("Type your message: \n").lower()
+            shift = int(input("Type the shift number: "))
+            text_arr= list(text)
+            ceaserCipher_word_arr = []
+            shift = -shift if encodeordecode == "decode" else shift
+            for letter in range(len(text_arr)):
+                for i in range(len(alphabet_letters)):
+                    if text_arr[letter] == alphabet_letters[i]:
+                        ceaserCipher_word_arr.append(alphabet_letters[i+shift])
+            print(f"{"".join(ceaserCipher_word_arr)}")
+            playAgain(isGameFinished)
+        else:
+           print("Please write either encode or decode")
+           isGameFinished = False
+
+# A function for cleaning terminal screen
 def clear_screen():
     # Windows
     if os.name == 'nt':
@@ -10,18 +44,6 @@ def clear_screen():
     else:
         subprocess.run('clear', shell=True)
 
-
-def playAgain(isGameFinished):
-    play_again = input("Type yes if you wanna play again, no for closing the game: ").lower()
-    while play_again != "yes" and play_again != "no":
-            play_again = input("Type yes if you wanna play again, no for closing the game: ").lower()
-    if play_again == "yes":
-            isGameFinished = False
-            clear_screen()
-    elif play_again == "no":
-            isGameFinished = True
-            sys.exit()
-            
 
             
 
